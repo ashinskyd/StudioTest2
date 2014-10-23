3c917954bed8caf2276fca41890b3fda77361fc8
David Ashinsky <ashinskyd@live.com>
2014-10-23 12:02:09 -0500

    David's Commit

--- app/src/main/java/com/test/gamify/myapplication/MyActivity.java
+++ app/src/main/java/com/test/gamify/myapplication/MyActivity.java
@@ -14,7 +14,7 @@ import android.os.Build;
 
 
 public class MyActivity extends Activity {
-    //Andy removed comment
+    //David removed comment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
